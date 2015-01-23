6c4b213cc81ec342b059e84a61e9b977268da009
sbb <601367322@qq.com>
2015-01-23 14:57:45 +0800

    new

--- app/src/main/java/com/xl/util/URLS.java
+++ app/src/main/java/com/xl/util/URLS.java
@@ -2,16 +2,16 @@ package com.xl.util;
 
 public class URLS {
 
-//    public static final String IP = "123.56.114.98";
-//	public static final String URL = "http://"+IP+"/";
+    public static final String IP = "123.56.114.98";
+	public static final String URL = "http://"+IP+"/";
 
-    public static final String IP="172.16.21.214";
-    public static final String URL="http://"+IP+":8080/XL_Product_service/";
+//    public static final String IP="172.16.21.214";
+//    public static final String URL="http://"+IP+":8080/XL_Product_service/";
 
 	public static final int PORT = 8181;
 	public static final String LAST=".do";
 	
-	public static final String SENDMESSAGE=URL+"b/sendmessage"+LAST;
+	public static final String SENDMESSAGE= URL +"b/sendmessage"+LAST;
 	public static final String JOINQUEUE=URL+"b/joinqueue"+LAST;
     public static final String EXITQUEUE=URL+"b/exitqueue"+LAST;
     public static final String CLOSECHAT=URL+"b/closechat"+LAST;
