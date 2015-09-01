f2fbe2e83731c298958892cf0639b0f340a261f5
shenbingbing <601367322@qq.com>
2015-09-01 23:12:22 +0800

    update

--- app/src/main/java/com/xl/activity/base/BaseBackActivity.java
+++ app/src/main/java/com/xl/activity/base/BaseBackActivity.java
@@ -28,47 +28,9 @@ public abstract class BaseBackActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
     }
 
-    int failNums = 0;
-
-    public void showScreenAd() {
-        if (MobclickAgent.getConfigParams(this, "ad_show").equals("on")) {
-            if(ac.cs.getISVIP() == CommonShared.OFF) {
-                adHandler.sendEmptyMessageDelayed(1, 25 * 1000);
-                adHandler.sendEmptyMessageDelayed(2, 30 * 1000);
-            }
-        }
-    }
-
-    Handler adHandler = new Handler() {
-        @Override
-        public void dispatchMessage(Message msg) {
-            switch (msg.what) {
-                case 1:
-                    if (!isFinishing()) {
-                        ToastUtil.toast(BaseBackActivity.this, getString(R.string.go_away_ad), R.drawable.stop);
-                    }
-                    break;
-                case 2:
-                    if (!isFinishing()) {
-                        DynamicSdkManager.getInstance(getApplicationContext()).showSpot(BaseBackActivity.this);
-                    }
-                    break;
-            }
-            super.dispatchMessage(msg);
-        }
-    };
-
-    @UiThread
-    public void showScreenAdFail() {
-        ToastUtil.toast(this, getString(R.string.i_will_come_back), R.drawable.weisuo);
-        showScreenAd();
-    }
 
     @Override
     protected void onDestroy() {
-        DynamicSdkManager.getInstance(getApplicationContext()).onDestroy(this);
         super.onDestroy();
-        adHandler.removeMessages(1);
-        adHandler.removeMessages(2);
     }
 }
