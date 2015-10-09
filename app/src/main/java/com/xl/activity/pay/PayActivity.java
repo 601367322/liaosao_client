1a8d394e6ef1240f0f1192c4f9720526e164551b
Your Name <you@example.com>
2015-10-09 14:19:09 +0800

    update

--- app/src/main/java/com/xl/activity/pay/PayActivity.java
+++ app/src/main/java/com/xl/activity/pay/PayActivity.java
@@ -16,6 +16,7 @@ import com.bmob.pay.tool.PayListener;
 import com.xl.activity.MainActivity;
 import com.xl.activity.R;
 import com.xl.activity.base.BaseBackActivity;
+import com.xl.util.LogUtil;
 import com.xl.util.MD5;
 import com.xl.util.ToastUtil;
 
@@ -41,17 +42,19 @@ public class PayActivity extends BaseBackActivity {
 
     @Click
     public void copy1() {
-        new BmobPay(PayActivity.this).pay(0.02, "一个月会员", new PayListener() {
+        new BmobPay(PayActivity.this).pay(0.01, "一个月会员", new PayListener() {
 
             // 因为网络等原因,支付结果未知(小概率事件),出于保险起见稍后手动查询
             @Override
             public void unknow() {
+                LogUtil.d("unknow");
                 ToastUtil.toast(mContext, "支付结果未知,请稍后手动查询");
             }
 
             // 支付成功,如果金额较大请手动查询确认
             @Override
             public void succeed() {
+                LogUtil.d("succeed");
                 ToastUtil.toast(mContext, "支付成功!");
             }
 
@@ -59,11 +62,13 @@ public class PayActivity extends BaseBackActivity {
             @Override
             public void orderId(String orderId) {
                 // 此处应该保存订单号,比如保存进数据库等,以便以后查询
+                LogUtil.d("orderId:\t" + orderId);
             }
 
             // 支付失败,原因可能是用户中断支付操作,也可能是网络原因
             @Override
             public void fail(int code, String reason) {
+                LogUtil.d("fail:\t" + code + "\t" + reason);
                 ToastUtil.toast(mContext, "支付中断!");
             }
         });
@@ -71,29 +76,32 @@ public class PayActivity extends BaseBackActivity {
 
     @Click
     public void copy2() {
-        new BmobPay(PayActivity.this).payByWX(0.02, "一个月会员", new PayListener() {
+        new BmobPay(PayActivity.this).payByWX(0.01, "一个月会员", new PayListener() {
 
             // 因为网络等原因,支付结果未知(小概率事件),出于保险起见稍后手动查询
             @Override
             public void unknow() {
+                LogUtil.d("unknow");
                 ToastUtil.toast(mContext, "支付结果未知,请稍后手动查询");
             }
 
             // 支付成功,如果金额较大请手动查询确认
             @Override
             public void succeed() {
+                LogUtil.d("succeed");
                 ToastUtil.toast(mContext, "支付成功!");
             }
 
             // 无论成功与否,返回订单号
             @Override
             public void orderId(String orderId) {
+                LogUtil.d("orderId:\t" + orderId);
             }
 
             // 支付失败,原因可能是用户中断支付操作,也可能是网络原因
             @Override
             public void fail(int code, String reason) {
-
+                LogUtil.d("fail:\t"+code+"\t"+reason);
                 // 当code为-2,意味着用户中断了操作
                 // code为-3意味着没有安装BmobPlugin插件
                 if (code == -3) {
@@ -120,6 +128,9 @@ public class PayActivity extends BaseBackActivity {
                                             copy1();
                                         }
                                     }).create().show();
+                }else if(code == 10077){
+                    BmobPay.ForceFree();
+                    copy2();
                 } else {
                     ToastUtil.toast(mContext, "支付中断!");
                 }
